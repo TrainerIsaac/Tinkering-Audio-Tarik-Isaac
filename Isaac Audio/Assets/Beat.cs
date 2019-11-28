615d3c11cf4a573b73b1974c7e8fdc17ab78601a
TrainerIsaac <isaaclouiswright@gmail.com>
2019-11-28 21:58:13 +0000

    Added guitar sound, rearranged and renamed scripts, added code so the main beat changes pitch and has the chance to play twice

--- Isaac Audio/Assets/Beat.cs
+++ Isaac Audio/Assets/Beat.cs
@@ -11,7 +11,8 @@ public class Beat : MonoBehaviour
     private int tempo;
     private float pitchChange;
     private int noteRange;
-
+    private int doubleNoteCounter;
+    private int raisePitch;
     void Start()
     {
         noteRange = random.Next(1,8); //Generates a power value for the pitch, to determine how many semitones will be added to the current value based on random choice
@@ -19,14 +20,30 @@ public class Beat : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         tempo = (random.Next(50, 60)); 
         audioSource.pitch = audioSource.pitch * pitchChange; //Multiplies frequency by pitchchange value
+
     }
-   
-    void Update()
+
+void Update()
     {
+        doubleNoteCounter = (random.Next(1, 3)); //doubleNoteCounter generates a random number between 1 and 3
+        raisePitch = random.Next(1, 3); //upAndDownPitch does the same
         counter++;
         if ((counter % tempo) == 0)
         {
+            if (raisePitch.Equals(1)) //if upAndDownPitch's randomly generated number is 1, the pitch will raise up half a note
+            {
+                audioSource.pitch = audioSource.pitch * (pitchChange / 2); //a full note was too radical a change to be done on average once every three beats.
+            }
             PlaySound();
+
+            if (doubleNoteCounter.Equals(1)) //if the number generated is equal to 1, then the PauseThenPlaySound function is used
+            {
+                StartCoroutine(PauseThenPlaySound());
+                PauseThenPlaySound();
+                StopCoroutine(PauseThenPlaySound());
+                audioSource.pitch = audioSource.pitch / (pitchChange / 2); //the current note is lowered by half a note to counteract the earlier pitch raise
+            }
+
         }
     }
 
@@ -35,4 +52,10 @@ public class Beat : MonoBehaviour
         audioSource.PlayOneShot(beat);
     }
 
+    IEnumerator PauseThenPlaySound() 
+    {
+        yield return new WaitForSeconds(pitchChange); //Waits for a number of seconds defined by the pitchChange value for a consistant beat to play
+
+        PlaySound();
+    }
 }
